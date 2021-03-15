da8ed1cde30315042dbc64e6fb29db4142fdf7ba
Adam <am1macdonald@gmail.com>
2021-03-15 19:49:54 -0400

    added function to increment the score & deliver victory message.

--- scripts/script.js
+++ scripts/script.js
@@ -4,40 +4,61 @@ function computerPlay() {
 
     return choices[Math.floor(Math.random() * choices.length - 1)];
 
-}
+};
 
 function game() {
+
+    let playerScore = 0;
+    let computerScore = 0;
+
+    function changeScore(winOrLose){
+        if (winOrLose == 'win') {
+            playerScore++
+            return "Win!"
+        } else if (winOrLose == 'lose'){
+            computerScore++
+            return "Lose..."
+        }
+    }
 
     function playRound(playerSelection, computerSelection) {
 
         if (playerSelection == 'rock') {
 
             return (computerSelection == 'rock') ? "Tie" 
-            :   (computerSelection == 'paper') ? "You lose"
-            :   "You win... This time.";
+            :   (computerSelection == 'paper') ? changeScore('lose')
+            :   changeScore('win');
         }
         else if (playerSelection == 'paper'){
             return (computerSelection == 'paper') ? "Tie! Great minds think alike!" 
-            :   (computerSelection == 'scissors') ? "LOSER!"
-            :   "You win the battle, not the war.";
+            :   (computerSelection == 'scissors') ? changeScore('lose')
+            :   changeScore('win');
         }
         else if (playerSelection == 'scissors'){
             return (computerSelection == 'scissors') ? "We chose ALIKE!" 
-            :   (computerSelection == 'rock') ? "Get a life!"
-            :   "Victory is yours.";
+            :   (computerSelection == 'rock') ? changeScore('lose')
+            :   changeScore('win');
         }
         else return "Please enter a valid option";
 
-    }   
+    };
+    
+    
 
     const playerSelection = prompt("Rock, paper, or scissors?");
     const computerSelection = computerPlay();
-    
+    console.log(playRound(playerSelection, computerSelection));
+    console.log(playerScore, computerScore);
+    console.log(playRound(playerSelection, computerSelection));
+    console.log(playerScore, computerScore);
+    console.log(playRound(playerSelection, computerSelection));
+    console.log(playerScore, computerScore);
+    console.log(playRound(playerSelection, computerSelection));
+    console.log(playerScore, computerScore);
+    console.log(playRound(playerSelection, computerSelection));
+    console.log(playerScore, computerScore);
 
-    for (let i = 0; i < 5; i++) {
-        console.log(playRound(playerSelection, computerSelection));
-    }
-}
+};
 
 
 
