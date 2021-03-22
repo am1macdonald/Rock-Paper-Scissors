bf3edbdf52846346f281abb2863e383a21e6ca63
Adam <am1macdonald@gmail.com>
2021-03-22 19:54:37 -0400

    added query selectors for buttons & got them interacting with the playRound() function.

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,17 @@
-const rockButton = document.querySelector;
+const buttons = document.querySelectorAll('button');
 
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playRound(button.id);
+    });
+});
 
-function computerPlay() {
-    const choices = ['rock', 'paper', 'scissors'];
-    return choices[Math.floor(Math.random() * choices.length)];
-};
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    let exitStrategy = false;
-    function changeScore(winOrLose){
+    
+let playerScore = 0;
+
+let computerScore = 0;
+let exitStrategy = false;
+function changeScore(winOrLose){
         if (winOrLose == 'w') {
             playerScore++
             return "Win!"
@@ -17,30 +19,42 @@ function game() {
             computerScore++
             return "Lose..."
         }
-    }
-    function playRound(playerSelection, computerSelection) {
-        if (/rock|^r/gi.test(playerSelection)) {
-            return (computerSelection == 'rock') ? "Tie" 
-            :   (computerSelection == 'paper') ? changeScore('l')
+}
+function computerPlay() {
+    const choices = ['rock', 'paper', 'scissors'];
+    return choices[Math.floor(Math.random() * choices.length)];
+};
+function playRound(playerSelection) {
+
+    const computerSelection = computerPlay();
+    
+
+    console.log('Player Score: ', playerScore);
+    console.log('Computer Score: ', computerScore);
+    console.log('comp: ' + computerSelection, 'player: ' + playerSelection);
+    
+
+    if (playerSelection === 'rock') {
+            return (computerSelection === 'rock') ? "Tie" 
+            :   (computerSelection === 'paper') ? changeScore('l')
             :   changeScore('w');
-        } else if (/paper|^p/gi.test(playerSelection)){
-            return (computerSelection == 'paper') ? "Tie! Great minds think alike!" 
-            :   (computerSelection == 'scissors') ? changeScore('l')
+    } else if (playerSelection === 'paper'){
+            return (computerSelection === 'paper') ? "Tie! Great minds think alike!" 
+            :   (computerSelection === 'scissors') ? changeScore('l')
             :   changeScore('w');
-        } else if (/scissors|^s/gi.test(playerSelection)){
-            return (computerSelection == 'scissors') ? "We chose ALIKE!" 
-            :   (computerSelection == 'rock') ? changeScore('l')
+    } else if (playerSelection === 'scissors'){
+            return (computerSelection === 'scissors') ? "We chose ALIKE!" 
+            :   (computerSelection === 'rock') ? changeScore('l')
             :   changeScore('w');
-        } else if (/exit|^e/gi.test(playerSelection)){
+    } else if (playerSelection){
             exitStrategy = true;
-        } else return "Please enter a valid option";
-    };
+    } else return "Please enter a valid option";
+};
+    /*
     while (playerScore < 5 && computerScore < 5 && exitStrategy == false){
         const playerSelection = prompt("Rock, paper, or scissors? (R, P, S)\n\nType Exit to quit.");
         const computerSelection = computerPlay();
         console.log(playRound(playerSelection, computerSelection), computerSelection);
         console.log(playerScore, computerScore);
     };
-    return (playerScore > computerScore) ? "You Win the Game!"
-    : "You Lose the Game!";
-};
+    */
