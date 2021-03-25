2ba4e120467df01ad10c5b333281b6d5c01a91d0
Adam <am1macdonald@gmail.com>
2021-03-25 05:58:37 -0400

    added reset & quit function to game

--- scripts/script.js
+++ scripts/script.js
@@ -3,30 +3,44 @@ let computerScore = 0;
 let tieCount = 0;
 let roundNumber = 0;
 
+var modal = document.getElementById("results-modal");
+
 const buttons = document.querySelectorAll('.game-button');
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         playRound(button.id);
-        updateScores();
-        
+        updateScores()        
     });
 });
 
-var modal = document.getElementById("results-modal");
+const newGameButton = document.getElementById("again");
+newGameButton.addEventListener("click", () => {
+
+    playerScore = 0;
+    computerScore = 0;
+    tieCount = 0;
+    roundNumber = -1;
+    buttons.forEach((button) => {
+        button.disabled = false;
+    })
+    updateScores();
+    modal.style.display = "none";
+});
+
+const quitGameButton = document.getElementById("quit");
+quitGameButton.addEventListener("click", () => {
+    modal.style.display = "none";
+});
 
 function endGame(){
     buttons.forEach((button)=>{
         button.disabled = true;
-    })
-
+    });
     if (playerScore > computerScore){
-        document.getElementById("announcement").innerText = "You Win!";
-        
+        document.getElementById("announcement").innerText = "You Win!";        
     } else document.getElementById("announcement").innerText = "You Lose...";
-
     modal.style.display = "block";
-
-}
+};
 
 function updateScores(){
     roundNumber++;
@@ -36,24 +50,16 @@ function updateScores(){
     document.getElementById("ties-display").innerText = tieCount.toString();
     if (playerScore === 5 || computerScore === 5) {
         endGame();
-    }
+    };
 };
 
 function computerPlay() {
     const choices = ['rock', 'paper', 'scissors'];
     return choices[Math.floor(Math.random() * choices.length)];
 };
+
 function playRound(playerSelection) {
-    
-
     const computerSelection = computerPlay();
-    
-
-    console.log('Player Score: ', playerScore.toString());
-    console.log('Computer Score: ', computerScore);
-    console.log('comp: ' + computerSelection, 'player: ' + playerSelection);
-    
-
     if (playerSelection === 'rock') {
         return (computerSelection === 'rock') ? tieCount++
                 : (computerSelection === 'paper') ? computerScore++
@@ -66,7 +72,5 @@ function playRound(playerSelection) {
         return (computerSelection === 'scissors') ? tieCount++
                 : (computerSelection === 'rock') ? computerScore++
                 : playerScore++;
-    };
-    
-    
+    };    
 };
