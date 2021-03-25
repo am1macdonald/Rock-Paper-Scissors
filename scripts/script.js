f0d5069ebc55dfbd900af5fb63ff7be8a735ab90
Adam <am1macdonald@gmail.com>
2021-03-24 22:19:29 -0400

    added buttons and a modal

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,46 @@
-const buttons = document.querySelectorAll('button');
-buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        playRound(button.id);
-    });
-});
-
-
 let playerScore = 0;
 let computerScore = 0;
 let tieCount = 0;
+let roundNumber = 0;
+
+const buttons = document.querySelectorAll('.game-button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playRound(button.id);
+        updateScores();
+        
+    });
+});
+
+var modal = document.getElementById("results-Modal");
+
+function endGame(){
+
+    if (playerScore > computerScore){
+        
+    } else alert("Loser!");
+
+    modal
+
+}
 
 function updateScores(){
+    roundNumber++;
+    document.getElementById("round-number").innerText = "ROUND: " + roundNumber.toString();
     document.getElementById("player-score-display").innerText = playerScore.toString();
     document.getElementById("computer-score-display").innerText = computerScore.toString();
     document.getElementById("ties-display").innerText = tieCount.toString();
+    if (playerScore === 5 || computerScore === 5) {
+        endGame();
+    }
+};
 
-};
-function incrementScore(player) {
-    if (player === 'comp') {
-        computerScore++;
-    } else if (player === 'person'){
-        playerScore++;
-    } else tieCount++;
-    updateScores();
-};
 function computerPlay() {
     const choices = ['rock', 'paper', 'scissors'];
     return choices[Math.floor(Math.random() * choices.length)];
 };
 function playRound(playerSelection) {
-    updateScores();
+    
 
     const computerSelection = computerPlay();
     
@@ -51,5 +62,7 @@ function playRound(playerSelection) {
         return (computerSelection === 'scissors') ? tieCount++
                 : (computerSelection === 'rock') ? computerScore++
                 : playerScore++;
-    }  
+    };
+    
+    
 };
